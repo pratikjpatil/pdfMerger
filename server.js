7c1b11885b68e13900e24a0fbad2fe4d4b5d9a20
Pratik <pratikpatil8560@gmail.com>
2022-11-14 15:18:00 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const multer  = require('multer');
+const {mergePdfs} = require('./merge');
+
+const upload = multer({ dest: 'uploads/' })
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/temp/index.html");
+})
+app.post('/merge', upload.array('pdfs', 12), async (req, res, next)=> {
+    await mergePdfs(__dirname+"/"+req.files[0].path,__dirname+"/"+req.files[1].path);
+    res.redirect("http://localhost:2000/static/merged.pdf");
+})
+
+app.listen(2000, ()=>{
+    console.log("Server started on port 2000");
+})
